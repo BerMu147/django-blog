6c048db7c9b8fab0f2d4de1180b1e8f18dfe4192
Berin Mujcinovic <berin.mujcinovic@mss.ba>
2023-09-26 19:47:51 +0200

    config URL parameters

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.articles),
+    re_path('(?P<slug>[\w-]+)/', views.article_detail)
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 # Create your views here.
 def articles(request):
    all_articles = Article.objects.all().order_by('date')
    return render(request, 'articles/article.html', {'all_articles':all_articles})
+
+def article_detail(request, slug):
+   return HttpResponse(slug)
